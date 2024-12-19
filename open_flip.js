73ce4c3c5a04c329bc3daf28f1b1e817b0d301a6
ahmadlaiq <ahmadlaiq97@gmail.com>
2024-12-19 13:35:14 +0700

    Add input and output JSON files for transfer data; update open_flip.js for improved element interaction

--- open_flip.js
+++ open_flip.js
@@ -22,14 +22,75 @@ const wdio = require('webdriverio');
   // Tunggu beberapa saat agar elemen dimuat
   await driver.pause(4000);
 
-  // Menggunakan locator XPath
-  const elementByXPath = await driver.$('//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup[3]');
+  // Menggunakan locator XPath untuk elemen Transfer
+  const elementTransferXPath = await driver.$('//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup[3]');
 
-  await elementByXPath.waitForDisplayed({ timeout: 5000 });
-  console.log("Elemen berdasarkan XPath tersebut ditemukan. Klik elemen...");
-  await elementByXPath.click();
+  await elementTransferXPath.waitForDisplayed({ timeout: 5000 });
+  console.log("Elemen berdasarkan XPath Transfer ditemukan. Klik elemen Transfer...");
+  await elementTransferXPath.click();
 
-  console.log("Elemen telah diklik.");
+  console.log("Elemen Transfer telah diklik.");
 
+  // Tunggu beberapa saat sebelum langkah berikutnya
+  await driver.pause(4000);
+
+  // Menggunakan locator XPath untuk elemen Banyak Rekening
+  const elementBanyakRekeningXPath = await driver.$('//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.view.ViewGroup[2]');
+
+  await elementBanyakRekeningXPath.waitForDisplayed({ timeout: 5000 });
+  console.log("Elemen berdasarkan XPath Banyak Rekening ditemukan. Klik elemen Banyak Rekening...");
+  await elementBanyakRekeningXPath.click();
+
+  console.log("Elemen Banyak Rekening telah diklik.");
+
+  // Tunggu beberapa saat sebelum langkah berikutnya
+  await driver.pause(4000);
+
+  // Menggunakan locator XPath untuk elemen Tujuan Baru
+  const elementTujuanBaruXPath = await driver.$('//android.widget.TextView[@text="Tujuan Baru"]');
+
+  await elementTujuanBaruXPath.waitForDisplayed({ timeout: 5000 });
+  console.log("Elemen berdasarkan XPath Tujuan Baru ditemukan. Klik elemen Tujuan Baru...");
+  await elementTujuanBaruXPath.click();
+
+  console.log("Elemen Tujuan Baru telah diklik.");
+
+  // Tunggu beberapa saat sebelum langkah berikutnya
+  await driver.pause(4000);
+
+  // Menggunakan locator XPath untuk elemen BRI / BRI VA
+  const elementBRIXPath = await driver.$('//android.widget.TextView[@text="BRI / BRI VA"]');
+
+  await elementBRIXPath.waitForDisplayed({ timeout: 5000 });
+  console.log("Elemen berdasarkan XPath BRI / BRI VA ditemukan. Klik elemen BRI / BRI VA...");
+  await elementBRIXPath.click();
+
+  console.log("Elemen BRI / BRI VA telah diklik.");
+
+  // Tunggu beberapa saat sebelum memasukkan nomor rekening
+  await driver.pause(4000);
+
+  // Menggunakan locator resource-id untuk field input nomor rekening
+  const elementNomorRekening = await driver.$('android=new UiSelector().resourceId("id.flip.staging:id/BENEFICIARY_ACCOUNT_SLIDER-TEXT_INPUT-BENEFICIARY_ACCOUNT")');
+
+  await elementNomorRekening.waitForDisplayed({ timeout: 5000 });
+  console.log("Field nomor rekening ditemukan. Memasukkan nomor rekening...");
+  await elementNomorRekening.setValue("123456789");
+
+  console.log("Nomor rekening berhasil dimasukkan.");
+
+  // Tunggu sebelum langkah berikutnya
+  await driver.pause(4000);
+
+  // Menggunakan locator resource-id untuk tombol Cek Rekening
+  const elementCekRekening = await driver.$('android=new UiSelector().resourceId("id.flip.staging:id/BENEFICIARY_ACCOUNT_SLIDER-TOUCHABLE_CHECK_ACCOUNT_NUMBER")');
+
+  await elementCekRekening.waitForDisplayed({ timeout: 5000 });
+  console.log("Tombol Cek Rekening ditemukan. Klik tombol Cek Rekening...");
+  await elementCekRekening.click();
+
+  console.log("Tombol Cek Rekening telah diklik.");
+
+  // Tunggu sebelum mengakhiri sesi
   await driver.pause(50000);
 })();
